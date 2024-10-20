00d699453b1c7ceb8198a44b6c3e7f74ac43acb9
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-21 00:09:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let output=document.querySelector("#output")
+let array=[1, 2, 3, ,5,4];
+let odd=setTimeout((array)=>{
+	array.filter((e)=>{
+		e%2!=0
+	})
+	
+},1000)
+
+output.innerText=odd;
