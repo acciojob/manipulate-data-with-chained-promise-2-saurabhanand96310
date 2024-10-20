96d2919e83a504a10f2e412eb04c35b5d9c92a34
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-21 00:44:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
+let output = document.querySelector(“#output”);
+let array = [1, 2, 3, 4];
+
 function getNumbers() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(array); // Resolves after 1 second
-    }, 1000);
-  });
+return new Promise((resolve) => {
+setTimeout(() => {
+resolve(array); // Resolves after 3 seconds
+}, 1000);
+});
+}
+
+function updateOutput(numbers) {
+output.innerText = numbers.join(", ");
 }
 
 getNumbers()
-  .then((numbers) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const evenNumbers = numbers.filter((n) => n % 2 === 0);
-        updateOutput(evenNumbers); // Output: 2, 4
-        resolve(evenNumbers);
-      }, 1000);
-    });
-  })
-  .then((evenNumbers) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const multipliedNumbers = evenNumbers.map((n) => n * 2);
-        updateOutput(multipliedNumbers); // Output: 4, 8
-        resolve(multipliedNumbers);
-      }, 1000);
-    });
-  });
+.then((numbers) => {
+return new Promise((resolve) => {
+setTimeout(() => {
+const evenNumbers = numbers.filter((n) => n % 2 === 0);
+updateOutput(evenNumbers); // Output: 2, 4
+resolve(evenNumbers);
+}, 1000);
+});
+})
+.then((evenNumbers) => {
+return new Promise((resolve) => {
+setTimeout(() => {
+const multipliedNumbers = evenNumbers.map((n) => n * 2);
+updateOutput(multipliedNumbers); // Output: 4, 8
+resolve(multipliedNumbers);
+}, 1000);
+});
+});
